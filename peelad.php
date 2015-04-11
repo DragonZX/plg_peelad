d6b0f597a404f9de6285439c20da7835665deffd
DragonZX <DragonZX@yandex.ru>
2015-04-11 20:01:56 +0300

    Javascript init

--- peelad.php
+++ peelad.php
@@ -2,7 +2,7 @@
 # @version $version 0.1 Amvis United Company Limited $
 # @copyright Copyright (C) 2014 AUnited Co Ltd. All rights reserved.
 # @license PeelAd has been originally created by Vitaliy Zhukov under GNU/GPL and relicensed under Apache v2.0, see LICENSE
-# Updated 1st December 2015
+# Updated 1st March 2015
 #
 # Site: http://aunited.ru
 # Email: info@aunited.ru
@@ -35,14 +35,38 @@ class plgSystemSunStat extends JPlugin
 		// Initialise variables
 		$peelad_enabled = $this->params->get( 'peelad_enabled', '' );
 		$peelad_jquery = $this->params->get( 'peelad_jquery', '' );
-		$peelad_path = $this->params->get( 'peelad_path', '' );
+		$peelad_path = $this->params->get( 'peelad_image', '' );
+		$peelad_link = $this->params->get( 'peelad_link', '' );
 		
 		switch($peelad_jquery) {
 			case 'google': $jqlink='//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'; break;
 			case 'local1': $jqlink='js/jquery-1.x.min.js'; break;
 			case 'local2': $jqlink='js/jquery-2.x.min.js'; break;
 			case 'external': $jqlink=''; break;
-			default: $jqlink='//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'; [break;]
+			default: $jqlink='//ajax.googleapis.com/ajax/libs/jquery/1.9.0/jquery.min.js'; break;
 		}
+		$javascript="<script src='".$jqlink."'></script>
+  <script src=\"jquery.peelback.js\"></script>  
+  <script>
+    $(function() {
+      $('body').peelback({
+        adImage  : 'images/peelads/peel-ad.png',
+        peelImage  : 'peel1.png',
+        clickURL : 'peelad_link',
+        smallSize: 50,
+        bigSize: 500,
+        gaTrack  : true,
+        gaLabel  : 'AUnited',
+        autoAnimate: true
+      });
+    });
+  </script>"
+		
+		$buffer = preg_replace ("/<\/body>/", $javascript."\n\n</body>", $buffer);
+		
+		//output the buffer
+		JResponse::setBody($buffer);
+		
+		return true;
 		
 ?>
